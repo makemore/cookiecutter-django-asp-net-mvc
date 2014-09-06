a0f753b6df114bcd1ba75186a6f11dbdde36f93f
Chris Barry <chris@chriss-mac-mini.config>
2014-09-06 15:35:20 +0100

    added sq lite & postgres commented out in seperate settings file

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -57,13 +57,6 @@ WSGI_APPLICATION = '{{cookiecutter.project_name}}.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings_local.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings_local.py
@@ -1,3 +1,14 @@
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -9,6 +20,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+"""
 
 USE_X_FORWARDED_HOST = False
 
