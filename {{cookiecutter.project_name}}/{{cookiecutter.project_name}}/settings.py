51159c7df004bb2348116615af25274e4df2f1c2
Chris Barry <chris@chriss-mac-mini.config>
2014-09-08 14:05:53 +0100

    Added allauth templates inside project

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -95,7 +95,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
+    os.path.join(BASE_DIR, "templates"),
 )
 
 # List of callables that know how to import templates from various sources.
