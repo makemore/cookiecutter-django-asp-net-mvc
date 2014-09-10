92198306823fdf5bd2a70684d73c4fab59252009
chrisbarry <chris.barry@makemoredigital.com>
2014-09-10 09:23:59 +0100

    added favicon

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.base import RedirectView
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^home/', include('home.urls')),
     (r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.ico'))
 )
