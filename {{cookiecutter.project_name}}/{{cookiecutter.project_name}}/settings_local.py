446c81f55b1c723df610fd315d45f44b96f9940c
chrisbarry <chris.barry@makemoredigital.com>
2014-09-14 22:08:37 +0100

    removed password & host settings from database file, so to use sockets

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings_local.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings_local.py
@@ -14,10 +14,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '{{cookiecutter.project_name}}',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': '', #below lines commented to connect via sockets, without a password. TCP connection requires password
+        #'PASSWORD': '',
+        #'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        #'PORT': '',                      # Set to empty string for default.
     }
 }
 """
